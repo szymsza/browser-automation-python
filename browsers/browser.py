eb7371a0a18c0b482956646be04eae9049626927
Jakub Szymsza <jakub@szymsza.cz>
2024-10-29 19:37:38 +0100

    Implement Chromium browser

--- browsers/browser.py
+++ browsers/browser.py
@@ -1,18 +1,29 @@
 from abc import ABC, abstractmethod
 from websockets.sync.client import ClientConnection
+import json
 
 class Browser(ABC):
 
-    ws_autoclose_delay: int
+    request_id: int = 0
+
+    ws_timeout: int
 
     ws_endpoint: str
     ws: ClientConnection
 
-    def __init__(self, browser_id: str = None, autoclose_timeout: int = 100, port: int = 9222, host: str = '127.0.0.1'):
-        self.ws_autoclose_delay = autoclose_timeout
+    def __init__(self, browser_id: str = None, autoclose_timeout: float = 0.1, port: int = 9222, host: str = '127.0.0.1'):
+        self.ws_timeout = autoclose_timeout
         self.initializeConnection(browser_id, port, host)
-        # TODO - rethink autoclose and method proxying wrt. Python
     
+    def req_id(self) -> int:
+        self.request_id += 1
+        return self.request_id
+    
+    def send(self, data: dict) -> dict:
+        data['id'] = self.req_id()
+        self.ws.send(json.dumps(data))
+        return json.loads(self.ws.recv(self.ws_timeout))
+
     # --- BROWSER-SPECIFIC METHODS ---
     # Initialize the browser connection
     @abstractmethod
--- browsers/chromium.py
+++ browsers/chromium.py
@@ -1,25 +1,64 @@
 from .browser import Browser
 from websockets.sync.client import connect
+from time import sleep
 
 class Chromium(Browser):
+    session_id: int
+
     def initializeConnection(self, browserId, port, host):
         self.ws_endpoint = f'ws://{host}:{port}/devtools/browser/{browserId}'
 
         self.ws = connect(self.ws_endpoint)
+        
+        target_response = self.send({
+            'method': 'Target.getTargets',
+        })
 
-        print(self.ws)
-        self.ws.send("Hello world!")
-        print("2")
-        message = self.ws.recv()
-        print(f"Received: {message}")
+        page_target = list(filter(lambda info: (info['type'] == 'page'), target_response['result']['targetInfos']))[0]['targetId']
 
-        pass
+        session = self.send({
+            'method': 'Target.attachToTarget',
+            'params': {
+                'targetId': page_target,
+                'flatten': True
+            }
+        })
+
+        self.session_id = session['params']['sessionId']
 
     def closeConnection(self):
         pass
 
     def navigate(self, url):
-        pass
+        self.send({
+            'sessionId': self.session_id,
+            'method': 'Page.navigate',
+            'params': {
+                'url': url
+            }
+        })
+        sleep(0.01)
 
     def click(self, x, y):
-        pass
+        self.send({
+            'sessionId': self.session_id,
+            'method': 'Input.dispatchMouseEvent',
+            'params': {
+                'x': x,
+                'y': y,
+                'type': 'mousePressed',
+                'clickCount': 1,
+                'button': 'left'
+            }
+        })
+
+        self.send({
+            'sessionId': self.session_id,
+            'method': 'Input.dispatchMouseEvent',
+            'params': {
+                'x': x,
+                'y': y,
+                'type': 'mouseReleased',
+                'button': 'left'
+            }
+        })
