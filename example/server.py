578af6079f255d36826bf343cccb6f7b4982f0c2
Jakub Szymsza <jakub@szymsza.cz>
2024-10-29 16:35:44 +0100

    Simple test server implementation

--- example/server.py
+++ example/server.py
@@ -6,10 +6,17 @@ port = 3000
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        if (self.path == '/redirected'):
+            print('Link clicked!')
+
         self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        # TODO - return the same result as with Node
-        self.wfile.write(b'Hello, World!')
+        self.wfile.write(f'<a href="http://{hostname}:{port}/redirected" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: red;">CLICK</a>'.encode())
+
+    # Ignore request logs
+    def log_request(self, code):
+        pass
 
 print(f"Starting server at http://{hostname}:{port}/")
 httpd = HTTPServer((hostname, port), SimpleHTTPRequestHandler)
